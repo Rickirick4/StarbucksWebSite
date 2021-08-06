d96fe3e633628413d0ab1dc13a19cc2847c861a9
Rickirick4 <71150839+Rickirick4@users.noreply.github.com>
2021-08-07 00:04:18 +0300

    Starbucks

---
+++ app.js
@@ -0,0 +1,8 @@
+function imgSlider(e) {
+    document.querySelector(".starbox").src = e;
+}
+
+function changeColor(e){
+    const circle = document.querySelector(".circle");
+    circle.style.background = e
+}
